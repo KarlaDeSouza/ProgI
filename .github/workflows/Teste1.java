af11f78e28d9a96cf687ea934c813d4fc1a9393f
KarlaDeSouza <92528691+KarlaDeSouza@users.noreply.github.com>
2021-10-14 11:18:12 -0300

    Create Teste1.java

---
+++ .github/workflows/Teste1.java
@@ -0,0 +1,10 @@
+// Classe de Teste de um arquivo Java aqui nesse site.
+public class Teste1
+{
+   // Método principal.
+   public static void main(String args[])
+   {
+       System.out.println(" Hello World! Isso aqui parece legal pra car****!");
+     
+   } // Fim do método principal.
+} // Fim da classe principal.
